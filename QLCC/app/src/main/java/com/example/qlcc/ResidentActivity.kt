b40baff7e45cd9a91f4bd078eacb8aeb54cd24af
Vel130 <hoangvinh20061306@gmail.com>
2026-04-18 21:04:01 +0700

    fix lỗi khoảng trắng

--- QLCC/app/src/main/java/com/example/qlcc/ResidentActivity.kt
+++ QLCC/app/src/main/java/com/example/qlcc/ResidentActivity.kt
@@ -1,36 +1,34 @@
 package com.example.qlcc
-​
  import android.os.Bundle
  import android.widget.Toast
  import androidx.appcompat.app.AppCompatActivity
  import androidx.recyclerview.widget.LinearLayoutManager
  import androidx.recyclerview.widget.RecyclerView
  import com.google.android.material.floatingactionbutton.FloatingActionButton
-​
  class ResidentActivity : AppCompatActivity() {
-​
+
      private lateinit var recyclerView: RecyclerView
      private lateinit var btnThem: FloatingActionButton
      private lateinit var dbHelper: DatabaseHelper
      private lateinit var adapter: ResidentAdapter
-​
+
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
          setContentView(R.layout.resident_list)
-​
+
          dbHelper = DatabaseHelper(this)
          recyclerView = findViewById(R.id.recyclerView)
          btnThem = findViewById(R.id.btnThem)
-​
+
          recyclerView.layoutManager = LinearLayoutManager(this)
          
          loadResidentList()
-​
+
          btnThem.setOnClickListener {
              Toast.makeText(this, "Tính năng Thêm cư dân sẽ được cập nhật", Toast.LENGTH_SHORT).show()
          }
      }
-​
+
      private fun loadResidentList() {
          val list = dbHelper.getAllUsers()
          adapter = ResidentAdapter(list, 
