2dfe237f0e8abca06794ca594c7dc7edb4c3876b
nguyenvinhtuanpk-dotcom <nguyenvinhtuanpk@gmail.com>
2026-04-20 10:59:49 +0700

    Update Report.kt

--- QLCC/app/src/main/java/com/example/qlcc/Report.kt
+++ QLCC/app/src/main/java/com/example/qlcc/Report.kt
@@ -1,12 +1,12 @@
 package com.example.qlcc
-
 import java.io.Serializable
 
+// PHẢI có chữ "data" ở phía trước
 data class Report(
     val reportId: Int,
     val userId: Int,
     val title: String,
     val content: String,
     val createdDate: String,
-    val status: String
+    var status: String // Để var để dễ cập nhật
 ) : Serializable
